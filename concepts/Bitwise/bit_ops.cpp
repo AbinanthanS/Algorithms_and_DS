eed1e30da971274e45658a6438df08b8e6cdb93a
Abinanthan <s.a.abinanthan2005@gmail.com>
2026-06-20 21:36:21 +0530

    file modified

--- concepts/Bitwise/bit_ops.cpp
+++ concepts/Bitwise/bit_ops.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 void check_ithbit(){
-    int a, i;
-    cin >> a >> i;
+    int a,i;
+    cin>>a>>i;
     if (a & (1<<i)) cout<<"Bit is set"; // (n>>i) & 1
     else cout<<"Bit is not set";
     cout<<"\n";
     return;
 }
 int set_ithbit(){
-    int a, i;
-    cin >> a >> i;
+    int a,i;
+    cin>>a>>i;
     return (a | (1<<i));
 }
 int clear_bit(){
